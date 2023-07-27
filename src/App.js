24738e51f83932207908fb79619b04ab917fa2ca
creativecomdevs <114491360+creativecomdevs@users.noreply.github.com>
2023-07-27 12:58:44 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Stake from "./pages/Stake";
 import Main from "./pages/Main";
-
+import { useStorageUpload } from "@thirdweb-dev/react";
 import { useAddress } from "@thirdweb-dev/react";
+import { MediaRenderer } from "@thirdweb-dev/react";
 
-export default function Home() {
+export default function App() {
   const address = useAddress();
+  const { mutateAsync: upload } = useStorageUpload();
+
+  const uploadData = async () => {
+    // Get any data that you want to upload
+    const dataToUpload = ['https://13a385f968380eef3358556f47ef07a7.ipfscdn.io/ipfs/bafybeifgvvbnnv4t26iwzqplyp5w6234swur6djjgfamd46j6fd4ll6d44/metamask.svg'];
+
+    // And upload the data with the upload function
+    const uris = await upload({ data: dataToUpload });
+  }
   if(!address) {
     return (
       <>
       <BrowserRouter>
+      {/* <MediaRenderer src="https://13a385f968380eef3358556f47ef07a7.ipfscdn.io/ipfs/bafybeifgvvbnnv4t26iwzqplyp5w6234swur6djjgfamd46j6fd4ll6d44/metamask.svg" /> */}
         <div className="noise">
           <main className="main">
           <Navbar/>
@@ -29,7 +40,9 @@ export default function Home() {
     )
   }
   return (
+
     <BrowserRouter>
+      {/* <MediaRenderer src="ipfs://QmamvVM5kvsYjQJYs7x8LXKYGFkwtGvuRvqZsuzvpHmQq9/0" /> */}
        <div className="noise">
           <main className="main">
           <Navbar/>
